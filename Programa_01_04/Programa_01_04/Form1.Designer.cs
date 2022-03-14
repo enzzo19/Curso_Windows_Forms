53f5f7043ae3df32ef573e45472779cf8fa1d5d8
Juarez Enzo <49879701+enzzo19@users.noreply.github.com>
2022-03-14 16:35:21 -0300

    add use to checkbox

---
+++ Programa_01_04/Programa_01_04/Form1.Designer.cs
@@ -0,0 +1,116 @@
+﻿
+namespace Programa_01_04
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.chk_monitor = new System.Windows.Forms.CheckBox();
+            this.chk_teclado = new System.Windows.Forms.CheckBox();
+            this.chk_mousse = new System.Windows.Forms.CheckBox();
+            this.lbl_message = new System.Windows.Forms.Label();
+            this.btn_calculate = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // chk_monitor
+            // 
+            this.chk_monitor.AutoSize = true;
+            this.chk_monitor.Location = new System.Drawing.Point(55, 52);
+            this.chk_monitor.Name = "chk_monitor";
+            this.chk_monitor.Size = new System.Drawing.Size(88, 17);
+            this.chk_monitor.TabIndex = 0;
+            this.chk_monitor.Text = "Monitor $250";
+            this.chk_monitor.UseVisualStyleBackColor = true;
+            this.chk_monitor.CheckedChanged += new System.EventHandler(this.chk_monitor_CheckedChanged);
+            // 
+            // chk_teclado
+            // 
+            this.chk_teclado.AutoSize = true;
+            this.chk_teclado.Location = new System.Drawing.Point(55, 75);
+            this.chk_teclado.Name = "chk_teclado";
+            this.chk_teclado.Size = new System.Drawing.Size(86, 17);
+            this.chk_teclado.TabIndex = 1;
+            this.chk_teclado.Text = "Teclado $15";
+            this.chk_teclado.UseVisualStyleBackColor = true;
+            // 
+            // chk_mousse
+            // 
+            this.chk_mousse.AutoSize = true;
+            this.chk_mousse.Location = new System.Drawing.Point(55, 99);
+            this.chk_mousse.Name = "chk_mousse";
+            this.chk_mousse.Size = new System.Drawing.Size(84, 17);
+            this.chk_mousse.TabIndex = 2;
+            this.chk_mousse.Text = "Mousse $20";
+            this.chk_mousse.UseVisualStyleBackColor = true;
+            // 
+            // lbl_message
+            // 
+            this.lbl_message.AutoSize = true;
+            this.lbl_message.Location = new System.Drawing.Point(22, 139);
+            this.lbl_message.Name = "lbl_message";
+            this.lbl_message.Size = new System.Drawing.Size(35, 13);
+            this.lbl_message.TabIndex = 3;
+            this.lbl_message.Text = "label1";
+            // 
+            // btn_calculate
+            // 
+            this.btn_calculate.Location = new System.Drawing.Point(220, 69);
+            this.btn_calculate.Name = "btn_calculate";
+            this.btn_calculate.Size = new System.Drawing.Size(75, 23);
+            this.btn_calculate.TabIndex = 4;
+            this.btn_calculate.Text = "Calcular";
+            this.btn_calculate.UseVisualStyleBackColor = true;
+            this.btn_calculate.Click += new System.EventHandler(this.btn_calculate_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(342, 215);
+            this.Controls.Add(this.btn_calculate);
+            this.Controls.Add(this.lbl_message);
+            this.Controls.Add(this.chk_mousse);
+            this.Controls.Add(this.chk_teclado);
+            this.Controls.Add(this.chk_monitor);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.CheckBox chk_monitor;
+        private System.Windows.Forms.CheckBox chk_teclado;
+        private System.Windows.Forms.CheckBox chk_mousse;
+        private System.Windows.Forms.Label lbl_message;
+        private System.Windows.Forms.Button btn_calculate;
+    }
+}
+
---
+++ Programa_01_04/Programa_01_04/Form1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Programa_01_04
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            lbl_message.Text = "";
+        }
+
+        private void btn_calculate_Click(object sender, EventArgs e)
+        {
+            int total = 0;
+            if (chk_monitor.Checked == true)
+                total = total + 250;
+            if (chk_teclado.Checked == true)
+                total = total + 15;
+            if (chk_mousse.Checked == true)
+                total = total + 20;
+            
+            // Esto es una ventana emergente
+            // MessageBox.Show("El total es: " + total.ToString());
+
+
+        }
+
+        private void chk_monitor_CheckedChanged(object sender, EventArgs e)
+        {
+            if (chk_monitor.Checked == true)
+                lbl_message.Text = "Monitor";
+            else
+                lbl_message.Text = "";
+        }
+    }
+}
