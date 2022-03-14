cba08864075c7c76115edc891f2647eeda45b311
Juarez Enzo <49879701+enzzo19@users.noreply.github.com>
2022-03-14 17:04:48 -0300

    add use to messagebox

---
+++ Programa_01_06/Programa_01_06/Form1.Designer.cs
@@ -0,0 +1,75 @@
+﻿
+namespace Programa_01_06
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn_mostrar = new System.Windows.Forms.Button();
+            this.lbl_message = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btn_mostrar
+            // 
+            this.btn_mostrar.Location = new System.Drawing.Point(76, 35);
+            this.btn_mostrar.Name = "btn_mostrar";
+            this.btn_mostrar.Size = new System.Drawing.Size(75, 23);
+            this.btn_mostrar.TabIndex = 0;
+            this.btn_mostrar.Text = "Mostrar";
+            this.btn_mostrar.UseVisualStyleBackColor = true;
+            this.btn_mostrar.Click += new System.EventHandler(this.btn_mostrar_Click);
+            // 
+            // lbl_message
+            // 
+            this.lbl_message.AutoSize = true;
+            this.lbl_message.Location = new System.Drawing.Point(92, 95);
+            this.lbl_message.Name = "lbl_message";
+            this.lbl_message.Size = new System.Drawing.Size(35, 13);
+            this.lbl_message.TabIndex = 1;
+            this.lbl_message.Text = "label1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(226, 186);
+            this.Controls.Add(this.lbl_message);
+            this.Controls.Add(this.btn_mostrar);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_mostrar;
+        private System.Windows.Forms.Label lbl_message;
+    }
+}
+
---
+++ Programa_01_06/Programa_01_06/Form1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Programa_01_06
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_mostrar_Click(object sender, EventArgs e)
+        {
+            // 1_solo con un texto
+            //MessageBox.Show("Hola a Todos :)");
+
+            // 2_ con texto y titulo en la ventana
+            // MessageBox.Show("Hola a Todos :)",
+            //                 "Mi messageBox");
+
+            // 3_ mensaje - titulo en ventana - botones
+            // MessageBox.Show("Probamos Botones",
+            //                 "Diferentes Botones",
+            //                 MessageBoxButtons.AbortRetryIgnore);
+
+            // 4_ mensaje - titulo en ventana - botones - iconos
+            MessageBox.Show("Probamos Botones",
+                            "Diferentes Botones",
+                            MessageBoxButtons.AbortRetryIgnore,
+                            MessageBoxIcon.Warning);
+
+            // Capturando lo que devuelve messagebox en una variable
+            DialogResult resultado = MessageBox.Show("Probamos Botones", "Diferentes Botones", MessageBoxButtons.AbortRetryIgnore);
+
+            // Tomando una decision basandonos en el resultado
+            if (resultado == DialogResult.Abort)
+                lbl_message.Text = "Omitir";
+            if (resultado == DialogResult.Retry)
+                lbl_message.Text = "Reintentar";
+            if (resultado == DialogResult.Ignore)
+                lbl_message.Text = "Ignorar";
+
+        }
+    }
+}
